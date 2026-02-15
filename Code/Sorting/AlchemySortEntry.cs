ed470623155f407a644164741d2a4e3d26ba161e
s ean <hses106021@gmail.com>
2026-02-15 13:23:19 +0800

    refactor: remove Array sorting implementation

--- Code/Sorting/AlchemySortEntry.cs
+++ Code/Sorting/AlchemySortEntry.cs
@@ -1,20 +1,21 @@
 // Copyright © TW-YTFeathered (https://github.com/TW-YTFeathered)
 // SeanOne™ - A Professional Project and Brand.
 
+using System;
 using System.Collections;
 
 namespace SeanOne.Alchemy.Sorting
 {
     internal partial class Sort
     {
-        public static void Entry(object obj, string ins)
+        public static void Entry<T>(T obj, string ins)
         {
             if (string.IsNullOrEmpty(ins))
             {
                 // 可以返回或拋出異常
                 return;
             }
-            
+
             if (obj is IList list)
             {
                 ListSorter.Sort(list, ins);
